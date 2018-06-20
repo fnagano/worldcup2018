ea1c4930d4941308b45bbd1271fcab70512cf77b
fnagano <fernando.nagano@advisiaanalytics.com>
2018-06-20 20:18:30 -0300

    Update groupStageSliders.js

--- Acompanhamento/groupStageSliders.js
+++ Acompanhamento/groupStageSliders.js
@@ -5,22 +5,22 @@ function groupStageSliders () {
 	var val = slider.value;
 	
 	// When the user scrolls the page, execute myFunction 
-	window.onscroll = function() {myFunction()};
+	//window.onscroll = function() {myFunction()};
 
 	// Get the header
 	//var header = document.getElementById("myHeader");
 
 	// Get the offset position of the navbar
-	var sticky = slider.offsetTop;
+	//var sticky = slider.offsetTop;
 
 	// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-	function myFunction() {
-  		if (window.pageYOffset >= sticky) {
-    		slider.classList.add("sticky");
-  		} else {
-    		slider.classList.remove("sticky");
-  		}
-	}
+	//function myFunction() {
+  	//	if (window.pageYOffset >= sticky) {
+    	//	slider.classList.add("sticky");
+  	//	} else {
+    	//	slider.classList.remove("sticky");
+  	//	}
+	//}
 	
 	
 
