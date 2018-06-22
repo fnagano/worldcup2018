d17b63fc1759c2ca41625b0941b27b11e700c5de
lucasnoveline <lucasnoveline@gmail.com>
2018-06-22 20:52:24 -0300

    Update stickSlider.js

--- Acompanhamento/stickSlider.js
+++ Acompanhamento/stickSlider.js
@@ -3,6 +3,7 @@ function stick()
   window.onscroll = function() {myFunction()};
 
   var header = document.getElementById("teste");
+  var text_init = document.getElementById("texto_inicial");
   var sticky = header.offsetTop + 200;
 
   function myFunction() {
@@ -10,8 +11,10 @@ function stick()
     console.log(window.pageYOffset);
     if (window.pageYOffset >= sticky) {
       header.classList.add("sticky");
+      text_init.classList.add("grande");
     } else {
       header.classList.remove("sticky");
+      text_init.classList.remove("grande");
     }
   }
 }
