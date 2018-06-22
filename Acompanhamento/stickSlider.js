c3d2bf186ccb22e00d2286cc8f1b3965e8e12bcd
lucasnoveline <lucasnoveline@gmail.com>
2018-06-21 21:23:58 -0300

    Create stickSlider.js

---
+++ Acompanhamento/stickSlider.js
@@ -0,0 +1,15 @@
+function stick() 
+{
+  window.onscroll = function() {myFunction()};
+
+  var header = document.getElementById("test");
+  var sticky = header.offsetTop;
+
+  function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+  }
+}
